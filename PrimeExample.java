38903eb1171b3cf840dccb0980df3e76545d400f
TOSEEB <79211097+TOSEEB@users.noreply.github.com>
2024-07-26 09:52:11 +0530

    ultimate success PrimeExample.java

--- PrimeExample.java
+++ PrimeExample.java
@@ -13,7 +13,7 @@ public class PrimeExample{
      break;      
     }      
    }      
-   if(flag==0)  { System.out.println(n+" is prime number"); }  
+   if(flag==0)  { System.out.println(n+" is yo prime number"); }  
   }//end of else  
 }    
 } 
