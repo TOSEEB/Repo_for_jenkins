298376aaade3bca8c3dab22a41280af1a2d869d3
TOSEEB <79211097+TOSEEB@users.noreply.github.com>
2024-07-26 09:47:14 +0530

    Update PrimeExample.java

--- PrimeExample.java
+++ PrimeExample.java
@@ -8,12 +8,12 @@ public class PrimeExample{
   }else{  
    for(i=2;i<=m;i++){      
     if(n%i==0){      
-     System.out.println(n+" is not prime number");      
+     System.out.println(n+" is not prime no");      
      flag=1;      
      break;      
     }      
    }      
-   if(flag==0)  { System.out.println(n+" is prime number"); }  
+   if(flag==0)  { System.out.println(n+" is prime no"); }  
   }//end of else  
 }    
 } 
